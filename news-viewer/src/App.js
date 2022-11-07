4bd6996f4f15d1528a02cd799cd47a8d5335dbbd
peanutBana <diao0321@gmail.com>
2022-11-07 12:18:08 +0900

    news-viewer

---
+++ news-viewer/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import {Route, Routes} from 'react-router-dom'
+import NewsPage from './pages/NewsPage';
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<NewsPage />}/>
+      <Route path='/:category' element={<NewsPage />}/>
+    </Routes>
+    
+  );
+}
+
+export default App;
---
+++ news-viewer/src/components/NewsItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsItem = () => {
+    return (
+        <div>
+            뉴스아이템
+        </div>
+    );
+}
+
+export default NewsItem;
